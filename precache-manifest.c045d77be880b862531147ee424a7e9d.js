40e6d34ddca11738f783a2bb5ab06f1c8a377b7f
Bruno Geraldi <bgeraldir@gmail.com>
2022-04-05 20:11:23 -0300

    Updates

---
+++ precache-manifest.c045d77be880b862531147ee424a7e9d.js
@@ -0,0 +1,82 @@
+self.__precacheManifest = [
+  {
+    "revision": "8c6638c3f32d9e24c34a",
+    "url": "/static-jobs-listings-new/static/css/main.30c47a6c.chunk.css"
+  },
+  {
+    "revision": "8c6638c3f32d9e24c34a",
+    "url": "/static-jobs-listings-new/static/js/main.8c6638c3.chunk.js"
+  },
+  {
+    "revision": "98fae294dabfb7543782",
+    "url": "/static-jobs-listings-new/static/js/1.98fae294.chunk.js"
+  },
+  {
+    "revision": "dffce846c8d5a7e5076c",
+    "url": "/static-jobs-listings-new/static/js/2.dffce846.chunk.js"
+  },
+  {
+    "revision": "06c9251eb4dc859a1804",
+    "url": "/static-jobs-listings-new/static/js/runtime~main.06c9251e.js"
+  },
+  {
+    "revision": "3f34b49c96dcbb44450b43184cd6cd78",
+    "url": "/static-jobs-listings-new/static/media/photosnap.3f34b49c.svg"
+  },
+  {
+    "revision": "fbe431a9e5d624447d698fee099ca843",
+    "url": "/static-jobs-listings-new/static/media/manage.fbe431a9.svg"
+  },
+  {
+    "revision": "9f72c13bc10a76e4bcb8f7ab62582b9f",
+    "url": "/static-jobs-listings-new/static/media/account.9f72c13b.svg"
+  },
+  {
+    "revision": "e0ad9358d008658c14d4350d48627923",
+    "url": "/static-jobs-listings-new/static/media/myhome.e0ad9358.svg"
+  },
+  {
+    "revision": "6c50fd745d8d5c5e1dd47fa8c9029a6b",
+    "url": "/static-jobs-listings-new/static/media/loop-studios.6c50fd74.svg"
+  },
+  {
+    "revision": "76faa79fba7225014706c0aac0b46472",
+    "url": "/static-jobs-listings-new/static/media/faceit.76faa79f.svg"
+  },
+  {
+    "revision": "45f63772b29b7ccba3bc0c0c6e1d505d",
+    "url": "/static-jobs-listings-new/static/media/shortly.45f63772.svg"
+  },
+  {
+    "revision": "52b85a5018745489a50626498bc347ad",
+    "url": "/static-jobs-listings-new/static/media/insure.52b85a50.svg"
+  },
+  {
+    "revision": "39eaebdf1b82df9648392a9fb0915e9c",
+    "url": "/static-jobs-listings-new/static/media/eyecam-co.39eaebdf.svg"
+  },
+  {
+    "revision": "23b515f95978bcb24d704b1abecc8196",
+    "url": "/static-jobs-listings-new/static/media/the-air-filter-company.23b515f9.svg"
+  },
+  {
+    "revision": "ae67c678a143e6c6b913629f3d3b55e9",
+    "url": "/static-jobs-listings-new/static/media/icon-remove.ae67c678.svg"
+  },
+  {
+    "revision": "db2678ac13d449eb19ffadef7c0eb44f",
+    "url": "/static-jobs-listings-new/static/media/Spartan-VariableFont_wght.db2678ac.ttf"
+  },
+  {
+    "revision": "776b89b486305b310ed0ef920280e780",
+    "url": "/static-jobs-listings-new/static/media/Spartan-Bold.776b89b4.ttf"
+  },
+  {
+    "revision": "6e2f7196c0644447b6887a48a265bdb1",
+    "url": "/static-jobs-listings-new/static/media/bg-header-desktop.6e2f7196.svg"
+  },
+  {
+    "revision": "df2f6310d3d161567893e5556fcda2d6",
+    "url": "/static-jobs-listings-new/index.html"
+  }
+];
